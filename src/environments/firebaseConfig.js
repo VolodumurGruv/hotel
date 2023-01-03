b2c66e308321abd5dae37a34e49936e5fd530153
VolodumurGrushka <gru.v@i.ua>
2023-01-03 18:13:56 +0200

    updated

---
+++ src/environments/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyBA7oedVRyrQBwK0xs6L_74HuWDiIrTrKk",
+	authDomain: "hotel-rooms-37041.firebaseapp.com",
+	projectId: "hotel-rooms-37041",
+	storageBucket: "hotel-rooms-37041.appspot.com",
+	messagingSenderId: "1016602076823",
+	appId: "1:1016602076823:web:724b2df9d7d2f17a216bd2",
+};
